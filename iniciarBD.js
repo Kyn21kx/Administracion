996df3afa9db0f74c0cb72c74b85d9745c86a774
JuanMGD <33186388+JuanMGD@users.noreply.github.com>
2018-05-13 17:19:51 -0500

    inicializa la BD

---
+++ iniciarBD.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBdUNe15L81jhoWaWJ_Y3WLctlw7RyXbRc",
+    authDomain: "admin-a8fdd.firebaseapp.com",
+    databaseURL: "https://admin-a8fdd.firebaseio.com",
+    projectId: "admin-a8fdd",
+    storageBucket: "admin-a8fdd.appspot.com",
+    messagingSenderId: "111450092120"
+  };
+  firebase.initializeApp(config);
