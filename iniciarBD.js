5e2279d044aeea4464c62d1cd1242888677c7262
Kyn21kx <nefesmeralda@gmail.com>
2018-05-18 01:06:19 -0500

    Conexión a la BD actualizada para conectar al nuevo server
    Conexión a la BD actualizada para conectar al nuevo server

--- iniciarBD.js
+++ iniciarBD.js
@@ -1,9 +1,9 @@
-var config = {
-    apiKey: "AIzaSyBdUNe15L81jhoWaWJ_Y3WLctlw7RyXbRc",
-    authDomain: "admin-a8fdd.firebaseapp.com",
-    databaseURL: "https://admin-a8fdd.firebaseio.com",
-    projectId: "admin-a8fdd",
-    storageBucket: "admin-a8fdd.appspot.com",
-    messagingSenderId: "111450092120"
+  var config = {
+    apiKey: "AIzaSyAsNA14-P2p6pkRqWhdGAtrn1Aqp5U3V88",
+    authDomain: "administracion-36586.firebaseapp.com",
+    databaseURL: "https://administracion-36586.firebaseio.com",
+    projectId: "administracion-36586",
+    storageBucket: "administracion-36586.appspot.com",
+    messagingSenderId: "488804713414"
   };
   firebase.initializeApp(config);
